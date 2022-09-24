79d89ab1d4d713ea4f947bd942d004190e27bb64
Luis Ariza <luismanuelariza@hotmail.com>
2022-09-24 17:37:01 -0500

    Integración de pug

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
+<template lang="pug">
+div
+	a(href="https://vitejs.dev" target="_blank")
+		img.logo(src="/vite.svg" alt="Vite logo")
+	a(href="https://vuejs.org/" target="_blank")
+		img.logo.vue(src="./assets/vue.svg" alt="Vue logo")
+HelloWorld(msg="Vite + Vue")
+</template>
+
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from "./components/HelloWorld.vue";
 </script>
-
-<template>
-	<div>
-		<a href="https://vitejs.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite logo" />
-		</a>
-		<a href="https://vuejs.org/" target="_blank">
-			<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-		</a>
-	</div>
-	<HelloWorld msg="Vite + Vue" />
-</template>
 
 <style scoped>
 .logo {
