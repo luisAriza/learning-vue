73bd5c84d8e9ac62a7daaadb47291f309f6d3b91
Luis Ariza <luismanuelariza@hotmail.com>
2022-09-26 14:53:25 -0500

    Estructura de un SFC

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,9 @@
 <template lang="pug">
-div
-	a(href="https://vitejs.dev" target="_blank")
-		img.logo(src="/vite.svg" alt="Vite logo")
-	a(href="https://vuejs.org/" target="_blank")
-		img.logo.vue(src="./assets/vue.svg" alt="Vue logo")
-HelloWorld(msg="Vite + Vue")
+HelloWorld
 </template>
 
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
-<style scoped>
-.logo {
-	height: 6em;
-	padding: 1.5em;
-	will-change: filter;
-}
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-	filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,7 @@
 <template lang="pug">
-h1 {{ msg }}
-.card
-	button(type="button" @click="count++") count is {{ count }}
-	p Edit
-		code components/HelloWorld.vue
-		| to test HMR
-p Check out
-	a(href="https://vuejs.org/guide/quick-start.html#local" target="_blank") create-vue
-	| , the official Vue + Vite starter
-p Install
-	a(href="https://github.com/johnsoncodehk/volar" target="_blank") Volar
-	| in your IDE for a better DX
-p.read-the-docs Click on the Vite and Vue logos to learn more
+p Component HelloWorld
 </template>
 
-<script setup>
-import { ref } from "vue";
+<script setup></script>
 
-defineProps({
-	msg: String,
-});
-
-const count = ref(0);
-</script>
-
-<style scoped>
-.read-the-docs {
-	color: #888;
-}
-</style>
+<style scoped></style>
