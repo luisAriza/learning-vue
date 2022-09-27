3ec422538411e02c526189520bb864993222e2ac
Luis Ariza <luismanuelariza@hotmail.com>
2022-09-26 20:39:54 -0500

    Creación de componentes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
-<template lang="pug">
-HelloWorld
-</template>
-
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+	import HelloWorld from "./components/HelloWorld.vue";
+	import Button from "./components/Button.vue";
 </script>
 
-<style scoped></style>
+<template lang="pug">
+.container
+	h1 Mi App
+	HelloWorld
+	Button
+</template>
+
+<style scoped>
+	.container {
+		max-width: 1280px;
+		margin: 0 auto;
+		padding: 2rem;
+		text-align: center;
+		border: 1px solid #1a1a1a;
+		border-radius: 8%;
+		transition-duration: 350ms;
+	}
+	.container:hover {
+		border-color: #f2f2f2;
+		transition-duration: 350ms;
+	}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,12 @@
-<template lang="pug">
-p Component HelloWorld
-</template>
-
 <script setup></script>
 
-<style scoped></style>
+<template lang="pug">
+h2 Hello World
+</template>
+
+<style scoped>
+	h2 {
+		font-size: 3.2em;
+		line-height: 1.1;
+	}
+</style>
