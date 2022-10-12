46f96bcc120f92edf1c0cb6d9c6f1786733b8d64
Luis Ariza <luismanuelariza@hotmail.com>
2022-10-12 17:30:21 -0500

    Componentes asincronos

--- platzi/composition-api/src/App.vue
+++ platzi/composition-api/src/App.vue
@@ -1,10 +1,14 @@
 <template>
 	<img alt="Vue logo" src="./assets/logo.png" />
-	<HelloWorld msg="Hello World!" />
+	<HelloWorld msg="Hello Vue!" />
 </template>
 
 <script>
-	import HelloWorld from "./components/HelloWorld.vue";
+	import { defineAsyncComponent } from "vue";
+
+	const HelloWorld = defineAsyncComponent(() =>
+		import("./components/HelloWorld.vue")
+	);
 
 	export default {
 		name: "App",
