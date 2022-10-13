b73226200943fa16a9bb714dc8761b1a89353b1e
Luis Ariza <luismanuelariza@hotmail.com>
2022-10-13 15:39:52 -0500

    Watch con composition API

--- platzi/composition-api/src/components/Home.vue
+++ platzi/composition-api/src/components/Home.vue
@@ -4,17 +4,26 @@
 </template>
 
 <script>
-	import { ref, reactive } from "vue";
+	import { ref, reactive, watch } from "vue";
 
 	export default {
 		setup() {
 			// Variables reactivas
 			const counter = ref(0);
 			setInterval(() => counter.value++, 500);
+			watch(counter, (value, old) => {
+				console.log(`REF: Act ${value}, Ant ${old}`);
+			});
 
 			// Variables y Objetos reactivos
 			const obj = reactive({ counter: 0 });
 			setInterval(() => obj.counter++, 500);
+			watch(
+				() => obj.counter,
+				(value, old) => {
+					console.log(`REACTIVE: Act ${value}, Ant ${old}`);
+				}
+			);
 
 			return {
 				counter,
