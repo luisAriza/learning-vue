86a8df09743136841579e8a2203d12b3624760ae
Luis Ariza <luismanuelariza@hotmail.com>
2022-10-13 17:58:46 -0500

    Provide/inject con composition API

--- platzi/composition-api/src/components/Home.vue
+++ platzi/composition-api/src/components/Home.vue
@@ -1,9 +1,10 @@
 <template>
 	<h3>Full name: {{ fullName }}</h3>
+	<h3>{{ username }}</h3>
 </template>
 
 <script>
-	import { toRefs, computed } from "vue";
+	import { toRefs, computed, inject } from "vue";
 
 	export default {
 		props: {
@@ -16,21 +17,23 @@
 				required: true,
 			},
 		},
-		// setup(props, { expose }) {
-		setup(props, context) {
-			console.log(context);
+		// setup(props, context) {
+		setup(props, { expose }) {
+			// console.log(context);
 
 			const { firstName, lastName } = toRefs(props);
 			const fullName = computed(() => {
 				return `${firstName.value} ${lastName.value}`;
 			});
+			const username = inject("username");
 
-			// expose({
-			// 	fullName,
-			// });
+			expose({
+				fullName,
+			});
 
 			return {
 				fullName,
+				username,
 			};
 		},
 	};
