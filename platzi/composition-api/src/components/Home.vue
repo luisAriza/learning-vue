32656f38e9e0d6f88da7ab887b781cd0ccf5363c
Luis Ariza <luismanuelariza@hotmail.com>
2022-10-13 15:47:56 -0500

    Computed con composition API

--- platzi/composition-api/src/components/Home.vue
+++ platzi/composition-api/src/components/Home.vue
@@ -1,33 +1,21 @@
 <template>
-	<h3>Con ref: {{ counter }}</h3>
-	<h3>Con reactive: {{ obj.counter }}</h3>
+	<h3>Full name: {{ fullName }}</h3>
 </template>
 
 <script>
-	import { ref, reactive, watch } from "vue";
+	import { ref, computed } from "vue";
 
 	export default {
 		setup() {
-			// Variables reactivas
-			const counter = ref(0);
-			setInterval(() => counter.value++, 500);
-			watch(counter, (value, old) => {
-				console.log(`REF: Act ${value}, Ant ${old}`);
+			const firstName = ref("Luis");
+			const lastName = ref("Ariza");
+
+			const fullName = computed(() => {
+				return `${firstName.value} ${lastName.value}`;
 			});
 
-			// Variables y Objetos reactivos
-			const obj = reactive({ counter: 0 });
-			setInterval(() => obj.counter++, 500);
-			watch(
-				() => obj.counter,
-				(value, old) => {
-					console.log(`REACTIVE: Act ${value}, Ant ${old}`);
-				}
-			);
-
 			return {
-				counter,
-				obj,
+				fullName,
 			};
 		},
 	};
