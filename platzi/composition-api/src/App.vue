9ce2a25d6f32426fa9d23e78cfedf0bbf17045a2
Luis Ariza <luismanuelariza@hotmail.com>
2022-10-13 11:31:49 -0500

    Hooks del ciclo de vida

--- platzi/composition-api/src/App.vue
+++ platzi/composition-api/src/App.vue
@@ -1,16 +1,26 @@
 <template>
-	<div>
-		<Modal />
-	</div>
+	<div>{{ text }}</div>
 </template>
 
 <script>
-	import Modal from "./components/Modal.vue";
-
 	export default {
 		name: "App",
-		components: {
-			Modal,
+		data() {
+			return {
+				text: "Hola Vue",
+			};
+		},
+		beforeCreated() {
+			// 'Data' aun no está disponible y 'El' tampoco
+			console.log("Before created", this.$data, this.$el);
+		},
+		created() {
+			// 'Data' ya está disponible pero 'El' aun no
+			console.log("Created", this.$data, this.$el);
+		},
+		mounted() {
+			// 'Data' y 'El' están disponible y montados
+			console.log("Mounted", this.$data, this.$el);
 		},
 	};
 </script>
@@ -19,6 +29,7 @@
 	#app {
 		font-family: Avenir, Helvetica, Arial, sans-serif;
 		color: #2c3e50;
+		text-align: center;
 		margin-top: 60px;
 	}
 </style>
