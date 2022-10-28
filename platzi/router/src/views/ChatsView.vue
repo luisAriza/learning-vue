69bfa08879ac7d7fd57a2625dd17097a27f1678a
Luis Ariza <luismanuelariza@hotmail.com>
2022-10-27 19:18:54 -0500

    Definición de rutas

---
+++ platzi/router/src/views/ChatsView.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <main>
+    <div>
+      <h2>Chats</h2>
+    </div>
+    <div>
+      <h2>Mensajes</h2>
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
--- platzi/router/src/views/HomeView.vue
+++ platzi/router/src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+  import { ref } from "vue";
+
+  const user = ref("");
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div>
+      <h1>Main</h1>
+      <input type="text" placeholder="Ingresar usuario" v-model="user" />
+      <p>Usuario: {{ user }}</p>
+    </div>
   </main>
 </template>
