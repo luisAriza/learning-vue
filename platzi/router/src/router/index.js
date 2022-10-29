3d6f3d7b2933dbd3054048be6bb5cc93ba41cfe4
Luis Ariza <luismanuelariza@hotmail.com>
2022-10-29 18:11:06 -0500

    Redirección y alias

--- platzi/router/src/router/index.js
+++ platzi/router/src/router/index.js
@@ -4,10 +4,19 @@ import HomeView from "@/views/HomeView.vue";
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
+		// Redireccion
+		// {
+		// 	path: "/home",
+		// 	redirect: {
+		// 		name: "home",
+		// 	},
+		// },
 		{
 			path: "/",
 			name: "home",
 			component: HomeView,
+			// Alias
+			alias: "/home",
 		},
 		{
 			path: "/session",
