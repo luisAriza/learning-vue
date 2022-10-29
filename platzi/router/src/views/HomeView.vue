9c9ea33191003404d9a04cea7e032567993941d5
Luis Ariza <luismanuelariza@hotmail.com>
2022-10-29 17:26:52 -0500

    Navegacion programatica

--- platzi/router/src/views/HomeView.vue
+++ platzi/router/src/views/HomeView.vue
@@ -1,15 +1,34 @@
 <script setup>
   import { ref } from "vue";
+  import { useRouter } from "vue-router";
 
+  const router = useRouter();
   const user = ref("");
+  const login = () => {
+    // router.push("/chats");
+
+    // router.push({
+    // path: "/chats",
+    // name: "chat",
+    // query: {
+    //   search: "arjona",
+    // },
+    // params: {
+    //   idChat: username.value,
+    // },
+    // });
+
+    // router.replace("/chats");
+
+    router.go(-1);
+  };
 </script>
 
 <template>
   <main>
-    <div>
-      <h1>Main</h1>
-      <input type="text" placeholder="Ingresar usuario" v-model="user" />
-      <p>Usuario: {{ user }}</p>
-    </div>
+    <h1>Main</h1>
+    <input type="text" placeholder="Ingresar usuario" v-model="user" />
+    <p>Usuario: {{ user }}</p>
+    <button @click="login">Iniciar sesión</button>
   </main>
 </template>
