38af766ee40da3ee635b069c97d7b1396fbc9010
Luis Ariza <luismanuelariza@hotmail.com>
2022-11-01 20:14:11 -0500

    Instalación y boilerplate de Vuex

--- platzi/vuex/src/main.js
+++ platzi/vuex/src/main.js
@@ -1,6 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import { Icon } from "@iconify/vue";
 
-import './assets/main.css'
+import "./assets/main.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+app.use(store);
+app.component("Icon", Icon);
+
+app.mount("#app");
