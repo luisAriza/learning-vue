b31aacf616ba00186071b0f37f6bb6d9cd191c0b
Luis Ariza <luismanuelariza@hotmail.com>
2022-11-16 11:52:36 -0500

    Setup inicial con pinia

---
+++ 05-pinia/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+	import { RouterView } from "vue-router";
+</script>
+
+<template>
+	<RouterView />
+</template>
+
+<style lang="postcss">
+	body {
+		@apply bg-neutral-900 text-gray-200;
+	}
+</style>
