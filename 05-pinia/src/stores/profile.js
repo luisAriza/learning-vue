ebc04301cb4d7df4141927b29789043fe83ce95a
Luis Ariza <luismanuelariza@hotmail.com>
2022-11-21 11:07:17 -0500

    Uso fuera de componentes con pinia

--- 05-pinia/src/stores/profile.js
+++ 05-pinia/src/stores/profile.js
@@ -3,8 +3,8 @@ import { defineStore } from "pinia";
 export default defineStore("profile", {
 	state: () => ({
 		id: 1,
-		username: "Ruisu Map",
+		username: "",
 		avatar: "/avatars/avatar.jpg",
-		status: "active",
+		status: null,
 	}),
 });
